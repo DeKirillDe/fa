872c206a2d146af1cde109b70f66e9c53bd31625
Kirill Kochetov <oggo3084@gmail.com>
2026-05-28 14:43:39 +0300

    Add user input for two integers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 int main() {
-    std::cout << "Programm started" << std::endl;
+    int a, b;
+    std::cout << "Введите первое целое число: ";
+    std::cin >> a;
+    std::cout << "Введите второе целое число: ";
+    std::cin >> b;
     return 0;
 }
